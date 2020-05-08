3b2630c5fecc9381e6157415fdcd22d14d34fcfd
Luca <lucabarendse@gmail.com>
2020-05-08 14:53:45 +0200

    same as last commit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,6 +34,7 @@ class ModelTests(TestCase):
             'test@ludicrux.com',
             'test123'
         )
-        
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+        
